52b8b3874efb0df4dee30922fd7bf258f0a35bf7
Michał Ochman <michal.ochman@grandparade.co.uk>
2013-12-26 22:55:26 +0100

    tweak static files

--- footer.php
+++ footer.php
@@ -1,4 +1,3 @@
-    <script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/onmediaquery.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
--- header.php
+++ header.php
@@ -25,20 +25,14 @@
 
         ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css" rel="stylesheet" media="screen">
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700&amp;subset=latin-ext" rel="stylesheet">
-    <link href="<?php echo get_template_directory_uri(); ?>/fonts/calendas/stylesheet.css" rel="stylesheet" media="screen">
-    <link href="<?php echo get_template_directory_uri(); ?>/css/screen.css" rel="stylesheet" media="screen">
-
+    <link href="<?php echo get_template_directory_uri(); ?>/screen.min.css" rel="stylesheet" media="screen">
     <!--[if lt IE 9]>
-    <link href="<?php echo get_template_directory_uri(); ?>/css/ie.css" rel="stylesheet" media="screen">
+    <link href="<?php echo get_template_directory_uri(); ?>/ie.min.css" rel="stylesheet" media="screen">
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
-
-    <script src="<?php echo get_template_directory_uri(); ?>/js/modernizr.custom.js"></script>
-
+    <script src="<?php echo get_template_directory_uri(); ?>/modernizr.min.js"></script>
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
     <?php wp_head(); ?>
