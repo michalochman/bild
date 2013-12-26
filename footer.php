9cd80dd3710e3b1b9ea2a6194872e70fde8c191f
Michał Ochman <michal.ochman@grandparade.co.uk>
2013-12-26 22:59:38 +0100

    use minimised js

--- footer.php
+++ footer.php
@@ -1,6 +1,4 @@
-    <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/js/onmediaquery.min.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/compiled.min.js"></script>
     <?php wp_footer(); ?>
 </body>
 </html>
